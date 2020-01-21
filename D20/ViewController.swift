64f5cd62d93c3dfd6c1c9c75b99f8431eb640059
Travis Towell <trt15b@acu.edu>
2020-01-21 13:48:45 -0600

    initial commit

---
+++ D20/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  D20
+//
+//  Created by Travis Towell on 1/21/20.
+//  Copyright © 2020 Travis Towell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        // its frigging gamer time
+    }
+
+
+}
+
