c96a3145996d3d660222b6521abca8902f3653ed
travis towell <trt15b@acu.edu>
2020-01-23 14:56:00 -0600

    last commit maybe

--- D20/ViewController.swift
+++ D20/ViewController.swift
@@ -7,15 +7,30 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     @IBOutlet weak var diceImageView: UIImageView!
+    @IBOutlet weak var criticalLabel: UILabel!
+    
+    var rollySounds: AVAudioPlayer?
+    var failSounds: AVAudioPlayer?
+    var winnySounds: AVAudioPlayer?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         // its frigging gamer time
+        let path = Bundle.main.path(forResource: "rolldice.mp3", ofType: nil)!
+        let url = URL(fileURLWithPath: path)
+        do {
+            rollySounds = try AVAudioPlayer(contentsOf: url)
+        }
+        catch {
+            //we are monkey
+        }
     }
 
     func rollDice() {
@@ -24,10 +39,32 @@ class ViewController: UIViewController {
         
         let imageName = "d\(rolledNumber)"
         
+
+        rollySounds?.play()
+        
         diceImageView.image = UIImage(named: imageName)
+        
+        if ( imageName == "d1"){
+            criticalLabel.text = "You're BAD!"
+            criticalLabel.isHidden = false
+        }
+        
+        else if( imageName == "d20")
+        {
+            criticalLabel.text = " You're not bad!"
+            criticalLabel.isHidden = false
+        }
+        
+        else{
+            criticalLabel.isHidden = true
+        }
         
     }
     
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event:
+        UIEvent?){
+        rollDice()
+    }
     @IBAction func rollDiceFromButton() {
         rollDice()
     }
